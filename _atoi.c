be1e516c07b16d8fc43091d87c8837a4b2c9ca6d
Divinelewis <divinitysoft0@gmail.com>
2022-12-16 10:39:05 -0800

    my git commit

--- _atoi.c
+++ _atoi.c
@@ -2,7 +2,7 @@
 
 /**
  * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * @info: struct address in shell
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
